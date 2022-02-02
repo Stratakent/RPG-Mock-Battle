b3e6490c2b4a282df4138fd13b41aa4d445e5055
Brian Nguyen <45743399+Stratakent@users.noreply.github.com>
2022-02-02 12:06:20 -0800

    Update battle.cpp

--- battle.cpp
+++ battle.cpp
@@ -6,23 +6,29 @@
 
 using namespace std;
 
+//idea is to be able to control your characters while the opposing enemy will be attacking and via "AI"
 void battle() {
-	queue<string> turn;
+	queue<string> turn;//setting up turn order possibly by speed.
+	
 	int action;
-	cout << "What will you do?\n1. Attack\n2. Command Arts\n3. Defend\n4. Item\n5. Flee\n Action: ";
+	bool friendly;
+	cout << "What will you do?\n1. Attack\n2. Command Arts\n3. Guard\n4. Item\n5. Flee\n Action: ";
 	cin >> action;
 	switch (action) {
 	case 1:
 		cout << "Character attacks!";
 		break;
 	case 2:
-
+		cout << "Character uses ______!";
+		break;
 	case 3:
-
+		cout << "Character Guards!"
+		break;
 	case 4:
-
+		cout << "Characters use an item!";
+		break;
 	case 5:
-
+		cout << "Character attempts to flee!";
 	default:
 		cout: "That is not a valid action!";
 		break;
